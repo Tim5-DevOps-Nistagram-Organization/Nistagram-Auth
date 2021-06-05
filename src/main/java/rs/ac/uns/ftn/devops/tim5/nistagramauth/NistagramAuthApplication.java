4bfb61936c5936cc1b3cacc239c8ad84262e808e
MicaTravica <40805992+MicaTravica@users.noreply.github.com>
2021-06-05 16:52:16 +0200

    init project #10

---
+++ src/main/java/rs/ac/uns/ftn/devops/tim5/nistagramauth/NistagramAuthApplication.java
@@ -0,0 +1,13 @@
+package rs.ac.uns.ftn.devops.tim5.nistagramauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NistagramAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NistagramAuthApplication.class, args);
+	}
+
+}
