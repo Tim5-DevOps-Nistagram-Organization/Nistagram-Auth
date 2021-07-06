9a12f6f63193ea6a97f5cfb1b5ebbc4ff210aeaa
DusanBucan <34024330+DusanBucan@users.noreply.github.com>
2021-07-06 12:00:43 +0200

    added tests (#23)

---
+++ src/test/java/rs/ac/uns/ftn/devops/tim5/nistagramauth/constants/UserConstants.java
@@ -0,0 +1,20 @@
+package rs.ac.uns.ftn.devops.tim5.nistagramauth.constants;
+
+public class UserConstants {
+
+    private UserConstants(){}
+
+    public static String USER_REGISTRATION_URI = "/user/registration";
+    public static String USER_VERIFICATION_URI = "/user/verify/";
+    public static String USER_LOGIN_URI = "/user/login";
+
+
+    public static String USER_SUCCESS_LOGIN_MESSAGE  = "You are registered, now you need to verify your email";
+
+    public static String USERNAME = "dusan";
+    public static String USERNAME_INVALID = "dusann";
+    public static String EMAIL = "dusanbzr@gmail.com";
+    public static String PASSWORD = "123456AAA@a";
+
+
+}
